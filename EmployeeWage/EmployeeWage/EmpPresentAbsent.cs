c7555c28910bce8c4054e87f1c7e436d76bfcc10
Satish Patil <satish.patil7017@gmail.com>
2022-08-21 16:31:30 +0530

    [Satish] Added Employee Present or Absent

---
+++ EmployeeWage/EmployeeWage/EmpPresentAbsent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//UC - 1 
+//Check Employee is Present or Absent
+//Use ((RANDOM)) for Attendance Check
+
+
+namespace Program
+{
+    public class EmpPresentAbsent
+    {
+        public int EmpPresent = 1;
+
+        public void CheckEmpPresentAbsent()
+        {
+            Random EmpCheck = new Random();
+            int value = EmpCheck.Next(0, 2);
+            if (value == EmpPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,13 @@
+﻿//Console.WriteLine("Welcome To Employee Wage Computation Problem");
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //UC1
+            EmpPresentAbsent prg = new EmpPresentAbsent();
+            prg.CheckEmpPresentAbsent();
+        }
+    }
+}
